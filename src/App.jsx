b98a0f9e4812f5179b308898275eca296bf30bb3
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-12-30 17:46:34 +0530

    add Home component to App and render Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './pages/Home/home'
 import Navbar from './components/Navbar/navbar'
 
 
 function App() {
     return(
+        <>
         <Navbar/>
+        <Home/>
+
+        </>
     )
 }
 

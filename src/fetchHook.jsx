fb175b5795df57bd2bb484d031558dec6cb815b6
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-01-02 00:29:31 +0530

    fix: format code in fetchHook.jsx for consistency

--- src/fetchHook.jsx
+++ src/fetchHook.jsx
@@ -2,7 +2,7 @@ import { useState,useEffect } from "react";
 
 export default function useFetch(url){
 
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 

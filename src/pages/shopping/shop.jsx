32ab4602ac57c78ed150dc519c76a07f7a79de38
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-01-02 21:46:56 +0530

    feat: create Shop component to display products with loading and error handling

---
+++ src/pages/shopping/shop.jsx
@@ -0,0 +1,28 @@
+import ProductCard from "../../components/productCard/productCard";
+import usefetch from '../../fetchHook';
+import styles from "./shop.module.css";
+
+export default function Shop({products,loading,error,quantities,updateQuantity,updateWishlist,removeFromWishlist,wishlist}) {
+
+
+    if(loading) return <div className={styles.loader}></div>
+
+    if(error) return <h2>Error when fetching: {error.message}</h2>
+    
+  return (
+    <div className={styles.container}>
+                {products.products.map((product) => (
+                    <ProductCard 
+                        key={product.id} 
+                        id = {product.id}
+                        product={product}
+                        quantity={quantities[product.id]}
+                        updateQuantity={updateQuantity}
+                        updateWishlist={updateWishlist}
+                        removeFromWishlist={removeFromWishlist}
+                        wishlist={wishlist}
+                    />
+                ))}
+    </div>
+  );
+}

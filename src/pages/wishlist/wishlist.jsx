2ccdcbc213a8662c827cb879238c5dc754f449c3
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-01-02 21:49:17 +0530

    feat: integrate ProductCard component into Wishlist for enhanced item display and quantity management

--- src/pages/wishlist/wishlist.jsx
+++ src/pages/wishlist/wishlist.jsx
@@ -1,8 +1,21 @@
-export default function Wishlist({}) {
+import ProductCard from "../../components/productCard/productCard"
+export default function Wishlist({wishBag,quantities,updateQuantity, updateWishlist,removeFromWishlist,wishlist}){
     return(
         <div>
             <h1>Items - Favorites</h1>
-
+            {wishBag.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        id={product.id}
+                        product={product}
+                        quantity={quantities[product.id]}
+                        updateQuantity={updateQuantity}
+                        updateWishlist={updateWishlist}
+                        removeFromWishlist={removeFromWishlist}
+                        wishlist ={wishlist}
+                    />
+        )
+    )}
         </div>
     )
 }

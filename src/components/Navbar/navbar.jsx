8252f46c1d6f98d415a57670dfaa55bdfb100376
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-01-03 01:08:50 +0530

    feat: add routing links to Navbar for Home, Shop, Wishlist, and Checkout

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,22 +1,22 @@
 import styles from './navbar.module.css'
 import { BsHeart } from "react-icons/bs";
 import { FiShoppingCart } from "react-icons/fi";
+import { Link } from 'react-router-dom';
 export default function Navbar(){
     return(
         <div className={styles.navbar}>
             <div className={styles.one}> 
                 <div >footwear.</div>
                 <div className={styles.link}>
-                    <div>Home</div>
-                    <div>Store</div>
+                    <div><Link to='/'>Home</Link></div>
+                    <div><Link to='/shop'>Shop</Link></div>
                 </div>
             </div>
             <div className={styles.icon}>
-                <div>
-                    <BsHeart/>
-                </div>
-                <div>
+                <div><Link to='/wishlist'><BsHeart/></Link></div>
+                <div><Link to='/checkout'>
                     <FiShoppingCart/>
+                    </Link>
                 </div>
             </div>
         </div>

05cdc72cc03b0e4cff03a2e08904f0cdcbd1e8ea
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-01-03 18:00:48 +0530

    feat: implement layout and total display for checkout page

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,7 +1,9 @@
 import ShopCard from "../../components/shopCard/shopCard"
+import styles from "./checkout.module.css"
 export default function Checkout({shopbag,quantities, updateQuantity}) {
    return(
-    <div>
+    <div className={styles.container}>
+        <div className={styles.cards}>
         {shopbag.map((product) => (
             <ShopCard 
                 key={product.id} 
@@ -10,6 +12,30 @@ export default function Checkout({shopbag,quantities, updateQuantity}) {
                 updateQuantity={updateQuantity}
             />
         ))}
+        </div>
+
+
+        {shopbag.length === 0 ? (
+            <div className={styles.empty}>Your cart is empty</div>
+        ) : (
+            <div className={styles.total}>
+                <h2>Order Summary</h2>
+                <div className={styles.subtotal}>
+                    <p>subtotal</p>
+                    <p>$0</p>
+                </div>
+                <div className={styles.subtotal}>
+                    <p>Delivery Fee</p>
+                    <p>$ 0</p>
+                </div>
+                <hr />
+                <div className={styles.subtotal}> 
+                    <p>Total</p>
+                    <p>$0</p>
+                </div>
+            </div>
+        )}
+
     </div>
    )
 }

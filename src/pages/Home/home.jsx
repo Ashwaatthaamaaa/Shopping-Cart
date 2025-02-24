2deaec26485cf922b280b2b6a6e2e478c49c35ad
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-02-25 02:30:34 +0530

    feat: add navigation link to Shop page from Home button

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,11 +1,15 @@
 import styles from "./home.module.css";
+import { Link } from 'react-router-dom';
+
 export default function Home(){
     return(
         <>
             <main className={styles.main}>
                 <h1>Welcome To footwear.</h1>
                 <p className={styles.tagline}>Step into style, walk with confidence</p>
-                <button className={styles.shopNow}>Shop Now</button>
+                <button className={styles.shopNow} >
+                    <Link to='/shop'>Shop Now</Link>
+                </button>
             </main>
         </>
     )

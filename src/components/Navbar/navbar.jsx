c09fe151f8259385856bd1e73c2aa35289814db6
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-03-25 23:54:22 +0530

    refactor: integrate Chakra UI into Navbar component, adding theme toggle functionality and improving icon layout

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,25 +1,50 @@
-import styles from './navbar.module.css'
-import { BsHeart } from "react-icons/bs";
-import { FiShoppingCart } from "react-icons/fi";
-import { Link } from 'react-router-dom';
-export default function Navbar({wishlistCount, shopbagCount}){
-    return(
-        <div className={styles.navbar}>
-            <div className={styles.one}> 
-                <div >footwear.</div>
-                <div className={styles.link}>
-                    <div><Link to='/'>Home</Link></div>
-                    <div><Link to='/shop'>Shop</Link></div>
-                </div>
-            </div>
-            <div className={styles.icon}>
-                <div><Link to='/wishlist'><BsHeart/>{wishlistCount}</Link></div>
-                <div><Link to='/checkout'>
-                    <FiShoppingCart/>
-                    {shopbagCount}
-                    </Link>
-                </div>
-            </div>
+"use client"
+
+import styles from "./navbar.module.css"
+import { BsHeart } from "react-icons/bs"
+import { FiShoppingCart, FiSun, FiMoon } from "react-icons/fi"
+import { Link } from "react-router-dom"
+import { useColorMode, IconButton } from "@chakra-ui/react"
+
+export default function Navbar({ wishlistCount, shopbagCount }) {
+  const { colorMode, toggleColorMode } = useColorMode()
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.one}>
+        <div className={styles.logo}>footwear.</div>
+        <div className={styles.link}>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/shop">Shop</Link>
+          </div>
         </div>
-    )
+      </div>
+      <div className={styles.icon}>
+        <IconButton
+          aria-label={`Switch to ${colorMode === "light" ? "dark" : "light"} mode`}
+          icon={colorMode === "light" ? <FiMoon /> : <FiSun />}
+          onClick={toggleColorMode}
+          variant="ghost"
+          size="md"
+          className={styles.themeToggle}
+        />
+        <div className={styles.iconWrapper}>
+          <Link to="/wishlist" className={styles.iconLink}>
+            <BsHeart className={styles.navIcon} />
+            {wishlistCount > 0 && <span className={styles.badge}>{wishlistCount}</span>}
+          </Link>
+        </div>
+        <div className={styles.iconWrapper}>
+          <Link to="/checkout" className={styles.iconLink}>
+            <FiShoppingCart className={styles.navIcon} />
+            {shopbagCount > 0 && <span className={styles.badge}>{shopbagCount}</span>}
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
+
